1a708ed836416b23ca0732d4ab354591501c6c5b
Kim Nguyen <design@kimisgold.com>
2020-04-24 08:50:06 -0400

    Attach css to all site views. (#7)

--- Module.php
+++ Module.php
@@ -37,34 +37,27 @@ class Module extends AbstractModule
 
     public function addCSS(Event $event) 
     {
-        $view = $event->getTarget();
-        $view->headLink()->appendStylesheet($view->url('site/css-editor', [
-            'site-slug' => $view->site->slug(),
-        ]));
-
-        $services = $this->getServiceLocator();
-        $siteSettings = $services->get('Omeka\Settings\Site');
-        $externalCss = $siteSettings->get('css_editor_external_css');
-        if ($externalCss) {
-            foreach ($externalCss as $uri) {
-                $view->headLink()->appendStylesheet($uri);
+        if ($this->getServiceLocator()->get('Omeka\Status')->isSiteRequest()) {
+            $view = $event->getTarget();
+            $view->headLink()->appendStylesheet($view->url('site/css-editor', [
+                'site-slug' => $view->site->slug(),
+            ]));
+    
+            $services = $this->getServiceLocator();
+            $siteSettings = $services->get('Omeka\Settings\Site');
+            $externalCss = $siteSettings->get('css_editor_external_css');
+            if ($externalCss) {
+                foreach ($externalCss as $uri) {
+                    $view->headLink()->appendStylesheet($uri);
+                }
             }
-        }
+        }      
     }
 
     public function attachListeners(SharedEventManagerInterface $sharedEventManager)
     {
-        $controllers = [
-            'Omeka\Controller\Site\Index',
-            'Omeka\Controller\Site\Item',
-            'Omeka\Controller\Site\ItemSet',
-            'Omeka\Controller\Site\Media',
-            'Omeka\Controller\Site\Page',
-        ];
 
-        foreach ($controllers as $controller) {          
-            $sharedEventManager->attach($controller, 'view.layout', [$this, 'addCSS']);
-        }
+        $sharedEventManager->attach('*', 'view.layout', [$this, 'addCSS']);
     }
 } 
 
